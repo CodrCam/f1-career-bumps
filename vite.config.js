d67a2982be284d88d320f29c5a2db811b27f5a9a
Cameron Griffin (He/Him) <ct.griffin7@gmail.com>
2025-05-14 21:25:50 -0700

    added github pages feature

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,26 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+
+  base: '/f1-career-bumps/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
+  build: {
+    outDir: 'dist', // default
+    emptyOutDir: true,
+  },
+
+  // GitHub Pages routing
+  server: {
+    historyApiFallback: true,
+    port: 5173,
+    open: true,
+  }
 });
